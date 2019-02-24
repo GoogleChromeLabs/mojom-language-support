0bf0ce4e968e3c3a17532ca0345a3787d4a4ddac
Kenichi Ishibashi <bashi@chromium.org>
2019-02-24 16:27:48 +0900

    Support more toplevel goto definition

--- server/src/definition.rs
+++ server/src/definition.rs
@@ -13,15 +13,34 @@ fn create_lsp_range(ast: &MojomAst, field: &mojom_parser::Range) -> Range {
 }
 
 pub fn find_definition(name: &str, uri: Url, ast: &MojomAst) -> Option<Location> {
+    macro_rules! match_field {
+        ($field:expr) => {{
+            let field_name = ast.text(&$field);
+            if field_name != name {
+                continue;
+            }
+            let range = create_lsp_range(ast, &$field);
+            return Some(Location::new(uri, range));
+        }};
+    }
+
+    // Only toplevel definitions for now.
     for stmt in &ast.mojom.stmts {
         match stmt {
-            Statement::Interface(ref intr) => {
-                let intr_name = ast.text(&intr.name);
-                if intr_name != name {
-                    continue;
-                }
-                let range = create_lsp_range(ast, &intr.name);
-                return Some(Location::new(uri, range));
+            Statement::Interface(ref stmt) => {
+                match_field!(stmt.name);
+            }
+            Statement::Struct(ref stmt) => {
+                match_field!(stmt.name);
+            }
+            Statement::Union(ref stmt) => {
+                match_field!(stmt.name);
+            }
+            Statement::Enum(ref stmt) => {
+                match_field!(stmt.name);
+            }
+            Statement::Const(ref stmt) => {
+                match_field!(stmt.name);
             }
             _ => (),
         }
