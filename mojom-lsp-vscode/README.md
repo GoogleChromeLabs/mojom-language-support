5200791308b60b39f3745fa5b327d323a61d58a5
Kenichi Ishibashi <bashi@chromium.org>
2019-05-19 15:43:16 +0900

    Add vscode extension

---
+++ mojom-lsp-vscode/README.md
@@ -0,0 +1,5 @@
+# VS Code extension for Chromium Mojom files
+
+This extension is a LSP client for Chromium mojom files.
+
+`npm install` then copy this directory to your [extension folder](https://vscode-docs.readthedocs.io/en/stable/extensions/install-extension/#your-extensions-folder).
---
+++ mojom-lsp-vscode/client/src/extension.ts
@@ -0,0 +1,35 @@
+import { workspace, ExtensionContext } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  Executable
+} from "vscode-languageclient";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  let serverOptions: Executable = {
+    command: "mojom-lsp-server"
+  };
+
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "mojom" }]
+  };
+
+  client = new LanguageClient(
+    "mojomLanguageServer",
+    "Mojom Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
