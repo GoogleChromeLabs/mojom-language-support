491e94909e77869c96d4e6df7efd52a1a8d18d06
Kenichi Ishibashi <bashi@chromium.org>
2020-04-12 17:01:51 +0900

    vscode: Support installing language server

--- vscode-mojom-idl/README.md
+++ vscode-mojom-idl/README.md
@@ -4,6 +4,4 @@ A Visual Studio Code extension for Mojom IDL.
 
 ## Setup
 
-If you only need syntax highlighting, just copy this directory to your [extension folder](https://code.visualstudio.com/api/working-with-extensions/publishing-extension#your-extension-folder).
-
-Optionally, run `npm install` before copying to get features from [mojom-lsp](../mojom-lsp).
+Run `npm install` then copy this directory to your [extension folder](https://code.visualstudio.com/api/working-with-extensions/publishing-extension#your-extension-folder).
--- vscode-mojom-idl/client/src/extension.ts
+++ vscode-mojom-idl/client/src/extension.ts
@@ -14,23 +14,27 @@
  * limitations under the License.
  */
 
-import { ExtensionContext } from "vscode";
+import * as child_process from "child_process";
+
+import * as vscode from "vscode";
 
 import {
   LanguageClient,
   LanguageClientOptions,
-  Executable
+  Executable,
 } from "vscode-languageclient";
+
+const SERVER_COMMAND = "mojom-lsp-server";
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
+function startClient() {
   let serverOptions: Executable = {
-    command: "mojom-lsp-server"
+    command: SERVER_COMMAND,
   };
 
   let clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: "file", language: "mojom" }]
+    documentSelector: [{ scheme: "file", language: "mojom" }],
   };
 
   client = new LanguageClient(
@@ -41,6 +45,68 @@ export function activate(context: ExtensionContext) {
   );
 
   client.start();
+}
+
+async function hasServerBinary(): Promise<boolean> {
+  return new Promise<boolean>((resolve) => {
+    const checkCommand = process.platform === "win32" ? "where" : "command -v";
+    const proc = child_process.exec(`${checkCommand} ${SERVER_COMMAND}`);
+    proc.on("exit", (code) => {
+      resolve(code === 0);
+    });
+  });
+}
+
+async function installServerBinary(): Promise<boolean> {
+  const task = new vscode.Task(
+    { type: "cargo", task: "install" },
+    vscode.workspace.workspaceFolders![0],
+    "Installing lsp server",
+    "mojom-lsp",
+    new vscode.ShellExecution("cargo install mojom-lsp")
+  );
+  const promise = new Promise<boolean>((resolve) => {
+    vscode.tasks.onDidEndTask((e) => {
+      if (e.execution.task === task) {
+        e.execution.terminate();
+      }
+    });
+    vscode.tasks.onDidEndTaskProcess((e) => {
+      resolve(e.exitCode === 0);
+    });
+  });
+  vscode.tasks.executeTask(task);
+
+  return promise;
+}
+
+async function tryToInstallLanguageServer(
+  configuration: vscode.WorkspaceConfiguration
+) {
+  const selected = await vscode.window.showInformationMessage(
+    "Install mojom-lsp-server (Rust toolchain required) ?",
+    "Install",
+    "Never"
+  );
+  if (selected === "Install") {
+    const installed = await installServerBinary();
+    if (installed) {
+      startClient();
+    }
+  } else if (selected === "Never") {
+    configuration.update("useLanguageServer", false);
+  }
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+  const configuration = vscode.workspace.getConfiguration("mojom");
+  const useLanguageServer = configuration.get<boolean>("useLanguageServer");
+  const shouldStartClient = useLanguageServer && (await hasServerBinary());
+  if (shouldStartClient) {
+    startClient();
+  } else if (useLanguageServer) {
+    tryToInstallLanguageServer(configuration);
+  }
 }
 
 export function deactivate(): Thenable<void> | undefined {
